60832c657174c31d93e9f910068974d236f19475
Michael Wilburn <michael-mw@hotmail.com>
2024-02-11 09:55:18 -0300

    M1-Chapter1  Introducing and Setting Up Go

---
+++ Module1/Chapter1/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+const (
+	Port = ":8080"
+)
+
+func serveDynamic(w http.ResponseWriter, r *http.Request) {
+	response := "The time is now " + time.Now().String()
+	fmt.Fprintln(w, response)
+}
+func serveStatic(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static.html")
+}
+
+// func serveError() {
+// 	fmt.Println("There's no way I'll work!")
+// }
+
+func main() {
+
+	http.HandleFunc("/static", serveStatic)
+	http.HandleFunc("/", serveDynamic)
+	// http.HandleFunc("/error", serveError)
+	log.Fatal(http.ListenAndServe(Port, nil))
+}
